edde30cc093514553a38232c62d85a4a33485554
Nikola <kojadinovic.nikola2205@gmail.com>
2020-03-04 18:20:21 +0100

    Histogram plotting feature added, file processing for csv and xlsx done

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(plotly)
 library(rsconnect)
 library(DT)
 
@@ -9,43 +10,90 @@ library(DT)
 
 
 
+
 shinyServer(function(input, output, session){
   
-  #REACTIVE FUNCTIONS FILE HANDLING
+  #####################################################################REACTIVE FUNCTIONS FILE HANDLING
   
   
-  file1 <- reactive({
-    input$file
-    
-  })
-  
-  
-  
+  # file1 <- reactive({
+  #   input$file
+  #   
+  # })
+
   
   data <- reactive({
     
-    if(is.null(file1())){
+    file1 <- input$file
+    if(is.null(file1)){
       return()
-      } else if(tools::file_ext(file1()$datapath) == "csv") {
-        read.table(file = file1()$datapath, 
+      } else if(tools::file_ext(file1) == "csv") {
+        read.csv(file = file1$datapath, 
                    sep = input$sep, 
                    header = input$header, 
                    skipNul = TRUE)
-      } else if (tools::file_ext(file1()$datapath) == "xlsx"){
-        readxl::read_excel(path = file1()$datapath)
+      } else if (tools::file_ext(file1) == "xlsx"){
+        readxl::read_xlsx(path = file1$datapath)
       }
   })
   
+  columns <- reactive({
+    names(data())
+  })
   
+  output$contents <- renderTable({
+    data()
+  })
   
+  ##################################################################### REACTIVE FUNCTIONS FILE HANDLING
   
  
+  
+  
+  
+  ##################################################################### DATA MENU OUTPUT - summary, spreadsheet, viz
+  
   output$table <- renderDataTable({
     if(is.null(data())){return()}
     data()
   })
   
- 
+  
+  output$struct <- renderPrint({
+    if(is.null(data())){return()}
+    summary(data())
+  })
+  
+  ################################################# VISUALIZATION PLOTS
+  
+  
+  output$inputwidget <- renderUI({
+    selectInput("col", "Select the column", choices = columns())
+  })
+  
+  output$inputwidget1 <- renderUI({
+    selectInput("cols1", "Select up to 3 variables", choices = columns(), multiple = TRUE)
+  })
+  
+  output$inputwidget2 <- renderUI({
+    selectInput("cols2", "Select multiple variables", choices = columns(), multiple = TRUE)
+  })
+    
+  
+  output$histogram <- renderPlot({
+    ggplot(data(), aes(x=unlist(data()[,input$col]))) + geom_histogram(bins = input$bins)
+  })
+  
+  
+  
+  
+  
+
+  
+  ################################################# VISUALIZATION PLOTS
+    
+  
+  ##################################################################### DATA MENU OUTPUT - summary, spreadsheet, viz
 })
   
   
--- ui.R
+++ ui.R
@@ -1,8 +1,10 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(plotly)
 library(rsconnect)
 library(DT)
+
 
 
 
@@ -19,17 +21,19 @@ shinyUI(
         menuItem("Upload options", tabName = "options", icon = icon("upload"),
                  menuSubItem(radioButtons("sep", "Separator",
                                           choices = c("Comma" = ",", "Tab" = "\t", "Space" = " ", "Semicolon" = ";"))),
-                        menuSubItem(radioButtons("nas", "Handle missing values", 
-                                                 choices = c("Omit rows with NAs", "Replace with mean", "Replace with median"))),
-                               menuSubItem(checkboxInput("header", "Header", value = FALSE))),
-       
+                 menuSubItem(radioButtons("nas", "Handle missing values", 
+                                          choices = c("Omit rows with NAs", "Replace with mean", "Replace with median"))),
+                 menuSubItem(checkboxInput("header", "Header", value = TRUE))),
+        
         menuItem("Data", tabName = "data", icon = icon("database"),
                  menuSubItem("Summary", tabName = "summary"),
-                        menuSubItem("Visualization", tabName = "viz")),
-       
+                 menuSubItem("Visualization", tabName = "viz"),
+                 menuSubItem("View spreadsheet", tabName = "spread")),
+        
         menuItem("Data preprocessing", tabName = "preprocess", icon = icon("desktop")),
-      
+        
         menuItem("About", tabName = "about", icon = icon("clipboard"))
+        
         
 
       )
@@ -39,10 +43,42 @@ shinyUI(
       
     ),
     
+    
+    
+    
     dashboardBody(
       tabItems(
 
-        tabItem(tabName = "about", dataTableOutput("table"))#povezujes sa inputom
+        tabItem(tabName = "summary", verbatimTextOutput("struct")),
+        
+      
+      
+        
+        tabItem(tabName = "spread", dataTableOutput("table")),
+        
+        tabItem(tabName = "viz",
+                fluidRow(
+                  
+                  box(title = "Histogram", uiOutput("inputwidget"), 
+                      sliderInput("bins", "Select number of bins", min = 10, max = 60, value = 20),
+                            plotOutput("histogram", height = 250)),
+                  
+                  box(title = "Line chart",uiOutput("inputwidget1")),
+                  
+                  box(title = "Scatterplot",uiOutput("inputwidget2"))
+                 
+                  
+                  
+                )),
+        
+        tabItem(tabName = "about", tableOutput("contents"))
+        
+        
+        
+        
+        
+        
+        
       )
       
       
